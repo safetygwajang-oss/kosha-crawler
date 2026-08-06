a03dd77c25017a500d9b58374c7106dfe2f39ae7
safetygwajang-oss <safetygwajang@gmail.com>
2026-08-06 16:56:04 +0900

    Update __init__.py

--- src/kosha_crawler/__init__.py
+++ src/kosha_crawler/__init__.py
@@ -1,5 +1,6 @@
 from .pipeline import crawl
+from .cafe_uploader import upload_pending
 from .config import settings
 
-__all__ = ["crawl", "settings"]
-__version__ = "1.0.0"
+__all__ = ["crawl", "upload_pending", "settings"]
+__version__ = "1.1.0"
